4c6b5c2acd4931ea5beb7d8c22835f81f9f426fd
Daiki007121 <dkoike@mail.sfsu.edu>
2022-11-27 20:19:17 +0900

    DONE

--- app.py
+++ app.py
@@ -54,4 +54,4 @@ def index3():
         return render_template("/index2.html")
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=8000, debug=True)
+    app.run(host="0.0.0.0", port=8000, debug=True)
