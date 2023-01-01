83f223f868a4925c3addc47df2543833cfe5b5bf
YK <you@example.com>
2023-01-01 15:16:09 +0000

    modified code

--- app.py
+++ app.py
@@ -5,17 +5,83 @@ import json
 import requests
 import secrets
 from flask import redirect, flash
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///Test.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO']=True
+
 # セッション情報を暗号化するためのキー設定
 app.secret_key = secrets.token_urlsafe(32)
+
+db = SQLAlchemy(app)
+
+class Quiz(db.Model):
+    __tablename__ = 'Quiz'
+    id = db.Column(db.Integer, primary_key=True)
+    question = db.Column(db.Text)
+    answer = db.Column(db.Text)
+
+@app.before_first_request
+def init():
+    db.create_all()
+
+@app.route('/create_question', methods=['GET'])
+def question_form():
+
+    return render_template('create_question.html')
+
+@app.route('/create_question', methods=['POST'])
+def insert_question():
+
+    question_str = request.form['question']
+    answer_str = request.form['answer']
+    dbs = Quiz(question=question_str, answer=answer_str)
+
+    db.session.add(dbs)
+    db.session.commit()
+
+    return render_template('create_question.html')
 
 
 @app.route("/")
 def index():
 
     return render_template("q1.html")
+
+
+@app.route("/answer", methods=["GET"])
+def view_question():
+    answer_flag = False
+    q = db.session.query(Quiz).first()
+    #print(q,q.question)
+    ids = q.id
+    quesion = q.question
+
+    return render_template("answer.html",answer_flag=answer_flag,question=quesion,idx=ids)
+
+
+@app.route("/answer", methods=["POST"])
+def answer():
+
+    q = request.form["question"]
+    p = request.form["answer"]
+    ids = int(request.form["idx"])
+
+    
+    d = db.session.query(Quiz).filter_by(question=q).first()
+
+    if p == d.answer:
+        ids += 1
+        answer_flag = True
+        d = db.session.query(Quiz).filter_by(id=ids).first()
+        quesion = d.question
+        return render_template("answer.html",answer_flag=answer_flag,question=quesion,idx=ids)
+    else:
+        flash("正解を入れてください")
+        return redirect("/answer")
 
 
 @app.route("/q2", methods=["POST"])
